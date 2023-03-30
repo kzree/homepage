b209f3e94325014dca9a25f61f37dcf040f2548d
Erkki Sula <43129445+kzree@users.noreply.github.com>
2023-03-30 21:02:40 +0300

    Add smooth scrolling, header link

--- src/components/homepage/Skills.tsx
+++ src/components/homepage/Skills.tsx
@@ -6,7 +6,7 @@ export const Skills = () => {
       <h2 className="text-3xl font-bold text-black mb-6">{majorSkills.title}</h2>
       {majorSkills.skills.map(({ description, title, icon: Icon }, idx) => (
         <div className="last:mb-0 mb-5 rounded-xl shadow-xl p-5" key={`skill-${idx}`}>
-          <div className="flex items-center gap-2 text-primary">
+          <div className="flex items-center gap-2 text-primary mb-2">
             <Icon size={40} />
             <h3 className="text-xl font-bold">{title}</h3>
           </div>
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -6,13 +6,22 @@ const inter = Inter({ subsets: ['latin'] });
 export const Header = () => {
   return (
     <header className={`h-16 w-full bg-primary ${inter.className}`}>
-      <nav className="container mx-auto h-full flex items-center">
+      <nav className="container mx-auto h-full flex items-center justify-between">
         <Link
           className="text-xl font-bold text-white hover:text-secondary transition-all duration-300 ease-in-out"
           href="/"
         >
           Erkki Sula
         </Link>
+        <div className="hidden md:block space-y-4">
+          <Link
+            scroll={false}
+            className="text-white hover:text-secondary transition-all duration-300 ease-in-out"
+            href="/#tech"
+          >
+            Technologies
+          </Link>
+        </div>
       </nav>
     </header>
   );
