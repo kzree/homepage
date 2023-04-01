cb691a0b830c7025cc0b315fdd63e34cf07ce16c
Erkki Sula <43129445+kzree@users.noreply.github.com>
2023-04-01 18:16:40 +0300

    Remove icon from skills

--- src/components/homepage/Skills.tsx
+++ src/components/homepage/Skills.tsx
@@ -10,7 +10,6 @@ export const Skills = () => {
       {skills.map(({ description, title, icon: Icon }, idx) => (
         <div className="last:mb-0 mb-5 rounded-xl shadow-xl p-5 bg-white" key={`skill-${idx}`}>
           <div className="flex items-center gap-2 text-primary mb-2">
-            <Icon size={40} />
             <h3 className="text-xl font-bold">{title}</h3>
           </div>
           <p>{description}</p>
