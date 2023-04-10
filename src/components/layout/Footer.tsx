bfe5c9b11ab78644c3c3bd273e98e79b2275ca9f
Erkki Sula <43129445+kzree@users.noreply.github.com>
2023-04-10 17:53:37 +0300

    Add link to source code to footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,11 +2,18 @@ import { footer } from '@/data/footer';
 import { Socials } from '@/components/common';
 
 export const Footer = () => {
+  const { copyright, sourceCode } = footer;
+
   return (
     <footer className="bg-gray">
       <div className="container mx-auto border-t border-secondary py-8">
         <div className="flex justify-between text-gray-light">
-          <p className="font-bold text-gray-light">{footer.copyright}</p>
+          <div className="flex gap-2">
+            <p className="font-bold text-gray-light">{copyright}</p>•
+            <a className="hover:text-secondary duration-300 ease-in-out" href={sourceCode.url}>
+              {sourceCode.text}
+            </a>
+          </div>
           <Socials className="text-gray-light" iconClassName="hover:text-secondary" />
         </div>
       </div>
--- src/data/footer.ts
+++ src/data/footer.ts
@@ -1,3 +1,7 @@
 export const footer = {
   copyright: `© Erkki Sula ${new Date().getFullYear()}`,
+  sourceCode: {
+    url: 'https://github.com/kzree/homepage',
+    text: 'Source code',
+  },
 };
